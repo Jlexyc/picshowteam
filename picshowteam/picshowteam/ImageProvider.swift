eed0366939ee042086a6a6e58ccae3c49cc5211b
Oleksii Kozulin <senior.jlexyc@gmail.com>
2019-01-14 11:15:05 +0200

    Initial commit

---
+++ picshowteam/picshowteam/ImageProvider.swift
@@ -0,0 +1,13 @@
+//
+//  ImageProvider.swift
+//  picshowteam
+//
+//  Created by Oleksii Kozulin on 1/14/19.
+//  Copyright © 2019 Oleksii Kozulin. All rights reserved.
+//
+
+import Foundation
+
+class ImageProvider {
+    
+}

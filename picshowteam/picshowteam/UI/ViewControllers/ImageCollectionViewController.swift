c8b004eaab67ef48b57adb2662c450db6c0168b3
Oleksii Kozulin <senior.jlexyc@gmail.com>
2019-01-15 02:06:49 +0200

    Added ImagePreview

--- picshowteam/picshowteam/UI/ViewControllers/ImageCollectionViewController.swift
+++ picshowteam/picshowteam/UI/ViewControllers/ImageCollectionViewController.swift
@@ -7,11 +7,26 @@
 //
 
 import UIKit
+import ImageViewer
 
-class ImageCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+extension UIImageView: DisplaceableView {}
+
+class ImageCollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, GalleryItemsDataSource, GalleryDisplacedViewsDataSource{
     
+    var galleryItems = [GalleryItem]()
     var images = [ImageModel]() {
         didSet {
+            self.galleryItems = self.images.map({ (model) -> GalleryItem in
+                return GalleryItem.image(fetchImageBlock: { (handler) in
+                    _ = ImageProvider.shared.getImage(model.bigImageUrl, completionHandler: { (image, error) in
+                        guard let safeImage = image else {
+                            print("one more error here")
+                            return;
+                        }
+                        handler(safeImage)
+                    })
+                })
+            })
             self.collectionView.reloadData()
         }
     }
@@ -46,8 +61,32 @@ class ImageCollectionViewController: UICollectionViewController, UICollectionVie
         return cell
     }
     
-    // MARK: FlowDelegate
+    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let configuration = [
+            GalleryConfigurationItem.deleteButtonMode(.none),
+            GalleryConfigurationItem.thumbnailsButtonMode(.none)
+        ]
+        
+        let galleryController = GalleryViewController(startIndex: indexPath.row, itemsDataSource: self, itemsDelegate: nil, displacedViewsDataSource: self, configuration: configuration)
+        self.present(galleryController, animated: true)
+    }
     
+    
+    // MARK: GalleryItemsDataSource & GalleryDisplacedViewsDataSource
+    
+    func itemCount() -> Int {
+        return self.galleryItems.count
+    }
+    
+    func provideGalleryItem(_ index: Int) -> GalleryItem {
+        return self.galleryItems[index]
+    }
+    
+    func provideDisplacementItem(atIndex index: Int) -> DisplaceableView? {
+        return (collectionView.cellForItem(at: IndexPath(row: index, section: 0)) as? ImageCell)?.imageView
+    }
+    
+    // MARK: FlowDelegate
     private let columns: CGFloat = 3
     
     private let sectionInsets = UIEdgeInsets(top: 50.0,
